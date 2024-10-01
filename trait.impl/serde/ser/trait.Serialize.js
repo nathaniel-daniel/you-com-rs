d249706f47ac42880bb3dbce06c5f05b49fc98bc
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-10-01 17:04:00 +0000

    deploy: c6914fcd817b8c4cfe54342c5305de717b9543b9

---
+++ trait.impl/serde/ser/trait.Serialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"you_com":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"you_com/struct.ChatMessage.html\" title=\"struct you_com::ChatMessage\">ChatMessage</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
